a9f4be9de132a1dc78a514ccd53261d0543888ca
scg16@126.com <scg16@126.com>
2018-09-29 18:25:22 +0800

    gateway

---
+++ laonog-gateway/src/main/java/com/laonog/gateway/LaonogGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.laonog.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaonogGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaonogGatewayApplication.class, args);
+	}
+}
---
+++ laonog-gateway/src/test/java/com/laonog/gateway/LaonogGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.laonog.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaonogGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
